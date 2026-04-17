acb66862a8630e35d76cab682bdf00f31dcae2c9
JorgePrez <jorge_operez@yahoo.com>
2026-04-17 08:04:04 -0600

    quitando streamlit feedback

--- asistente_miu_compras.py
+++ asistente_miu_compras.py
@@ -12,7 +12,7 @@ from langsmith import Client
 
 from langsmith.run_helpers import get_current_run_tree
 
-from streamlit_feedback import streamlit_feedback
+#from streamlit_feedback import streamlit_feedback
 
 from langchain_core.callbacks import collect_runs
 
